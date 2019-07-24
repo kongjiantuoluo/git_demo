e7701f3c0f2f0f3f05da09151dbce9efea796ee2
kongjiantuoluo <Tuoluo8163@163.com>
2019-07-24 10:22:57 +0800

    提交打印版本

--- src/main/java/com/git/git_demo/Test.java
+++ src/main/java/com/git/git_demo/Test.java
@@ -2,6 +2,6 @@ package com.git.git_demo;
 
 public class Test {
 	public static void main(String[] args) {
-		System.out.println("hello world");
+		System.out.println("hello world version 2.0 ");
 	}
 }
